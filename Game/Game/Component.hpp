c6e5f6057cf2b7a56c64af1bb114152f5ae5b0e3
Leapt-Takuya <takuya01@feel.ocn.ne.jp>
2016-06-26 16:44:27 +0900

    [update] Component Classの完成
    完成しました

--- Game/Game/Component.hpp
+++ Game/Game/Component.hpp
@@ -8,11 +8,19 @@ private:
 
 public:
   Component();
+  Component(int width, int height);
   ~Component();
 
-  init();
-
+  virtual void init(void);
+  virtual void display(void);
   virtual void keyboard(unsigned char key, int x, int y);
+  virtual void resize(int w, int h);
+  virtual void mouse(int button, int state, int x, int y);
+  virtual void keyboard(unsigned char key, int x, int y);
+  virtual void keyboardup(unsigned char key, int x, int y);
+  virtual void special(int key, int x, int y);
+  virtual void specialup(int key, int x, int y);
+  virtual void idle(void);
 };
 
 #endif
