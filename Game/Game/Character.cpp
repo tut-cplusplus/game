aa579d4c74db07323b37354788e96d89c15bda5f
F0AIN <FRain7.ag2S00@gmail.com>
2016-06-26 20:27:35 +0900

    [updata] テクスチャIDの配列をポインタに変更
    テクスチャIDをポインタに変更し，初期化時にサイズを指定できる
    更に初期化関数initを追加

--- Game/Game/Character.cpp
+++ Game/Game/Character.cpp
@@ -1,15 +1,46 @@
 #include "Character.h"
 
-Character::Character(int x, int y) : x(x),y(y),direction(WEST) {
+Character::Character(int x, int y,int size) : x(x),y(y),direction(WEST) {
+	this->texture_id = new int[size];
 }
 
 
 Character::~Character() {
+	delete texture_id;
+}
+
+void Character::init(int x, int y, int size) {
+	this->x = x;
+	this->y = y;
+	if (this->texture_id != nullptr) {
+		delete this->texture_id;
+	}
+	this->texture_id = new int[size];
 }
 
 
 void Character::face(Direction d) {
 	this->direction = d;
+}
+
+void Character::move(void) {
+	switch (direction)
+	{
+	case NORTH:
+		y++;
+		break;
+	case SOUTH:
+		y--;
+		break;
+	case EAST:
+		x++;
+		break;
+	case WEST:
+		x--;
+		break;
+	default:
+		break;
+	}
 }
 
 void Character::breakWall(void) {
