af9930b3a19ea12021d74b7f8a8df052eafd6286
capacitypp <aoi0x4c4f5645@gmail.com>
2016-06-26 14:05:43 +0900

    [add] BlockクラスへのisTransparentの追加
    透明ブロックかどうかを返す

--- Game/Game/Block.h
+++ Game/Game/Block.h
@@ -11,6 +11,13 @@ private:
 	int height;	/* 高さ */
 public:
 	/**
+	 * 透明ブロックかどうか．
+	 *
+	 * @return true 透明である場合
+	 * @return false 透明でない場合
+	 */
+	virtual bool isTransparent(void) const;
+	/**
 	 * ブロックの破壊時に呼ばれる．将来的にブロックアイテムのドロップやアニメーションインスタンスの生成を行う．
 	 */
 	virtual void destroy(void) = 0;
@@ -28,6 +35,11 @@ public:
 	void setHeight(int height);
 
 };
+
+bool Block::isTransparent(void) const
+{
+	return false;
+}
 
 inline int Block::getWidth(void) const
 {
