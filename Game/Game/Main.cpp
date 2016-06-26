20406c819fdec033749647302c798a0614bc3a51
capacitypp <aoi0x4c4f5645@gmail.com>
2016-06-26 10:31:48 +0900

    [add] glMatrixModeを使用するように変更
    display関数側でウィンドウサイズを考慮しなくてよい

--- Game/Game/Main.cpp
+++ Game/Game/Main.cpp
@@ -161,8 +161,10 @@ void visibility(int state)
 void resize(int w, int h)
 {
 	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(0.0, 640.0, 0.0, 480.0, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
 }
 
 
