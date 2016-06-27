01c7fd39969f3504f48c1d15584fc88d41455700
capacitypp <aoi0x4c4f5645@gmail.com>
2016-06-28 07:41:14 +0900

    [add] ComponentTeapotのソースコードを分割

--- Game/Game/ComponentTeapot.hpp
+++ Game/Game/ComponentTeapot.hpp
@@ -1,8 +1,6 @@
 #ifndef ___Class_ComponentTeapot
 #define ___Class_ComponentTeapot
 
-#include "GL/glut.h"
-
 #include "Component.hpp"
 
 /**
@@ -19,34 +17,6 @@ public:
 
 	virtual void draw(void);
 };
-
-ComponentTeapot::ComponentTeapot()
-{
-
-}
-
-ComponentTeapot::ComponentTeapot(int width, int height)
-	: angle(0), Component(width, height)
-{
-
-}
-
-void ComponentTeapot::draw(void)
-{
-	int size = (width < height) ? width : height;
-	glColor3d(1.0, 1.0, 1.0);
-	glBegin(GL_LINE_LOOP);
-	glVertex2d(0.0, 0.0);
-	glVertex2d(width, 0.0);
-	glVertex2d(width, height);
-	glVertex2d(0.0, height);
-	glEnd();
-	glPushMatrix();
-	glTranslated(width / 2.0, height / 2.0, 0.0);
-	glRotated(angle++, 0.0, 1.0, 0.0);
-	glutWireTeapot(size / 3.5);
-	glPopMatrix();
-}
 
 #endif
 
