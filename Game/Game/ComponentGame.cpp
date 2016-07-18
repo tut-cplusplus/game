7c267eaa6cef63cdb78546911a52b8a216c6c576
F0AIN <FRain7.ag2S00@gmail.com>
2016-07-18 15:09:14 +0900

    [fix] VSにてComponentGame.cppのblock初期化の修正など
    ComponentGame.cppにて
    blockをswitch文で初期化を行っていた部分を１行修正．
    また，BlockTrap.hpp，BlockTrap.cppをプロジェクトに追加

--- Game/Game/ComponentGame.cpp
+++ Game/Game/ComponentGame.cpp
@@ -321,6 +321,7 @@ void ComponentGame::placeBlock(const vector<Player*> players)
 			block = new BlockTrap(blockSize);
 			break;
 		case Player::DECOY:
+		default:
 			block = nullptr;
 			break;
 		}
