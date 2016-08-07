d56a65d8e2ff5834517c34a35fa00341b4af5403
capacitypp <aoi0x4c4f5645@gmail.com>
2016-08-07 16:34:42 +0900

    [update] 深さ優先探索の高速化
    Graphをポインタとして受け取るようにした
    速度を重視してこのように変更したが、Graphのインスタンスが外部に存在するため、最終的には別の方法にするべき

--- Game/Game/Tree.hpp
+++ Game/Game/Tree.hpp
@@ -8,7 +8,7 @@
 template <typename T>
 class Tree : public Graph<T> {
 	std::stack<int> indexes;
-	Graph<T> graph;
+	const Graph<T>* graph;
 	int iterationNumber;
 public:
 	Tree(int iterationNumber);
@@ -41,7 +41,7 @@ void Tree<T>::depthFirstSearch(const Graph<T>& graph, unsigned idx)
 	indexes = std::stack<int>();
 	Graph<T>::nodes.clear();
 	Graph<T>::adjacencyMatrix.clear();
-	this->graph = graph;
+	this->graph = &graph;
 	Graph<T>::addNode(graph.getNodes()[idx]);
 	indexes.push(idx);
 }
@@ -65,13 +65,13 @@ void Tree<T>::update(void)
 			break;
 		unsigned idx = indexes.top();
 		indexes.pop();
-		const T& node = graph.getNodes()[idx];
-		auto row = graph.getAdjacencyMatrix()[idx];
+		const T& node = graph->getNodes()[idx];
+		auto row = graph->getAdjacencyMatrix()[idx];
 		for (unsigned i = 0; i < row.size(); i++) {
 			unsigned char value = row[i];
 			if (!value)
 				continue;
-			const T& newNode = graph.getNodes()[i];
+			const T& newNode = graph->getNodes()[i];
 			try {
 				Graph<T>::searchNode(newNode);
 			}
