4f278cd50020aa31fcafe7c6ea204e9606a77ce8
capacitypp <aoi0x4c4f5645@gmail.com>
2016-08-09 22:39:13 +0900

    [update] マップのダンプにおいて，ブロック数を出力する機能を追加

--- Game/Game/Debug.cpp
+++ Game/Game/Debug.cpp
@@ -14,6 +14,7 @@ void Debug::dump(const std::string& fpath, Block*** map)
 	ofs << " 0 : BlockAir" << endl;
 	ofs << " 1 : BlockNormalWall" << endl;
 	ofs << " 2 : BlockTrap" << endl;
+	unsigned count[4] = {0};
 	for (int i = 1; i < Global::MAP_HEIGHT - 1; i++) {
 		for (int j = 1; j < Global::MAP_WIDTH - 1; j++) {
 			Block* blockPtr = map[i][j];
@@ -27,9 +28,19 @@ void Debug::dump(const std::string& fpath, Block*** map)
 				else
 					type = 2;
 			}
+			count[type + 1]++;
 			ofs << to_string(type);
 		}
 		ofs << endl;
 	}
+	ofs << "nullptr          : " << count[0] << endl;
+	ofs << "BlockAir         : " << count[1] << endl;
+	ofs << "BlockNormalWall  : " << count[2] << endl;
+	ofs << "BlockTrap        : " << count[3] << endl;
+	unsigned sum = 0;
+	for (unsigned i = 0; i < sizeof(count) / sizeof(count[0]); i++)
+		sum += count[i];
+	ofs << "sum              : " << sum << endl;
+	ofs << "correct map size : " << (Global::MAP_HEIGHT - 2) * (Global::MAP_WIDTH - 2) << endl;
 }
 
